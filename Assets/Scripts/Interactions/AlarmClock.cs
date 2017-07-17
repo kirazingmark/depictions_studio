f1d521007f80b227b36a1d26719e7769ba59cc56
Kira Zingmark <kirazingmark@gmail.com>
2017-07-18 06:21:26 +1000

    Shower Interaction Update.
    - Shower Particle Effects & Sounds Implemented.
    - TestScript9 (to be adapted for Shower Interaction) copied into Scripts folder.
    - Other minor changes.

---
+++ Assets/Scripts/Interactions/AlarmClock.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlarmClock : MonoBehaviour
+{
+
+    public Rigidbody rb;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.constraints = RigidbodyConstraints.FreezeAll;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Interactions/TestScript9.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// PARTICLESYSTEM ON-OFF CONTROLLER.
+    // PLACE THIS SCRIPT ON THE PARTICLESYSTEM IN THE SCENE THAT WILL TOGGLE ON & OFF.
+
+public class TestScript9 : MonoBehaviour
+{
+
+    // CONSTANTS & VARIABLES.
+
+    public ParticleSystem vent;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        vent = GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Thrust button pressed, start particle system
+        if (Input.GetKeyDown("u"))
+        {
+            PlayThrusters();
+        }
+        // Thrust button released, deactivate particle system
+        if (Input.GetKeyDown("j"))
+        {
+            StopThrusters();
+        }
+    }
+
+    void PlayThrusters()
+    {
+        vent.Play();
+        vent.enableEmission = true;
+    }
+    void StopThrusters()
+    {
+        vent.enableEmission = false;
+        vent.Stop();
+    }
+}
+
