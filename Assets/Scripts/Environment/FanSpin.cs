9527e3d549059bb9a194142181fb2e12e4751332
Kira Zingmark <kirazingmark@gmail.com>
2017-07-18 00:03:41 +1000

    Minor Interactions Update.
    - Doors implemented in Scene (Temporary Sounds included).
    - Door Frames added around Doors.
    - Glass Doors for Showers created and implemented in Scene.
    - Ceiling Fans implemented in Scene and set to spin from new 'FanSpin' Script.

---
+++ Assets/Scripts/Environment/FanSpin.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FanSpin : MonoBehaviour {
+
+    // CONSTANTS AND VARIABLES.
+    public float rotation = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+            transform.Rotate(0, rotation * Time.deltaTime, 0);
+    }
+}
