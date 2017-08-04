3016b776ff76d14d95bc4db6d4b391a53d90ba6d
Kira Zingmark <kirazingmark@gmail.com>
2017-08-04 20:10:26 +1000

    Camera System Update.
    - Empty GameObject created to store all Cameras from the Main Scene in (except the primary Camera attached to the Player).
    - 5 Additional Cameras created, positioned around the level and added to the new Cameras GameObject.
    - CameraSwitcher Script scrubbed and rewritten. Now simply set up to switch between Cameras using the Alpha1 - Alpha6 keys (for Debug purposes only, later build will use a function call after a RayCast on an chair object trigger).
    - New 'Camera' Tag setup.
    - Other minor changes.

--- Assets/Scripts/Interactions/CameraSwitcher.cs
+++ Assets/Scripts/Interactions/CameraSwitcher.cs
@@ -5,76 +5,88 @@ using UnityEngine;
 public class CameraSwitcher : MonoBehaviour {
 
     // CONSTANTS & VARIABLES.
-    public Camera MainCamera;
-    public Camera SofaCamera;
-    private bool sofaActive;
+    public Camera Camera1; // Main Camera on Player.
+    public Camera Camera2; // Sofa Camera.
+    public Camera Camera3; // EnSuite Toilet Camera.
+    public Camera Camera4; // Main Toilet Camera.
+    public Camera Camera5; // Desk Camera.
+    public Camera Camera6; // Dining Table Camera.
 
     // Use this for initialization
-    void Start () {
-
-        MainCamera.enabled = true;
-        SofaCamera.enabled = false;
-    }
-	
-	// Update is called once per frame
-	void Update () {
-
-        if (sofaActive)
-        {
-
-            // Sit Down.
-            MainCamera.enabled = false;
-            SofaCamera.enabled = true;
-        }
-        else
-        {
-
-            // Get Up.
-            MainCamera.enabled = true;
-            SofaCamera.enabled = false;
-        }
-
-        if (Input.GetButtonDown("E") && sofaActive)
-        {
-            sofaActive = !sofaActive;
-        }
-        else if (Input.GetButtonDown("E") && !sofaActive)
-        {
-            // Nothing.
-        }
-        else
-        {
-            // Nothing.
-        }
-    }
-
-    void OnGUI()
+    void Start()
     {
 
-        if (sofaActive)
-        {
-
-            GUI.Label(new Rect(Screen.width / 2 - 75, Screen.height - 100, 350, 80), "<color=white><size=35>Sit/Get Up - 'E'</size></color>");
-        }
+        Camera1.enabled = true; // Main Camera on Player enabled by default, all others disabled at runtime.
+        Camera2.enabled = false;
+        Camera3.enabled = false;
+        Camera4.enabled = false;
+        Camera5.enabled = false;
+        Camera6.enabled = false;
     }
 
-    //Activate the Main function when player is near the Sofa.
-    void OnTriggerEnter(Collider other)
+    // Update is called once per frame
+    void Update()
     {
 
-        if (other.gameObject.tag == "Player")
+        // Camera 1.
+        if (Input.GetKeyDown(KeyCode.Alpha1) && (Camera2.enabled == true || Camera3.enabled == true || Camera4.enabled == true || Camera5.enabled == true || Camera6.enabled == true))
         {
-            sofaActive = true;
+            Camera1.enabled = true;
+            Camera2.enabled = false;
+            Camera3.enabled = false;
+            Camera4.enabled = false;
+            Camera5.enabled = false;
+            Camera6.enabled = false;
         }
-    }
-
-    //Deactivate the Main function when player is away from Sofa.
-    void OnTriggerExit(Collider other)
-    {
-
-        if (other.gameObject.tag == "Player")
+        // Camera 2.
+        else if (Input.GetKeyDown(KeyCode.Alpha2) && (Camera1.enabled == true || Camera3.enabled == true || Camera4.enabled == true || Camera5.enabled == true || Camera6.enabled == true))
         {
-            sofaActive = false;
+            Camera1.enabled = false;
+            Camera2.enabled = true;
+            Camera3.enabled = false;
+            Camera4.enabled = false;
+            Camera5.enabled = false;
+            Camera6.enabled = false;
+        }
+        // Camera 3.
+        else if (Input.GetKeyDown(KeyCode.Alpha3) && (Camera1.enabled == true || Camera2.enabled == true || Camera4.enabled == true || Camera5.enabled == true || Camera6.enabled == true))
+        {
+            Camera1.enabled = false;
+            Camera2.enabled = false;
+            Camera3.enabled = true;
+            Camera4.enabled = false;
+            Camera5.enabled = false;
+            Camera6.enabled = false;
+        }
+        // Camera 4.
+        else if (Input.GetKeyDown(KeyCode.Alpha4) && (Camera1.enabled == true || Camera2.enabled == true || Camera3.enabled == true || Camera5.enabled == true || Camera6.enabled == true))
+        {
+            Camera1.enabled = false;
+            Camera2.enabled = false;
+            Camera3.enabled = false;
+            Camera4.enabled = true;
+            Camera5.enabled = false;
+            Camera6.enabled = false;
+        }
+        // Camera 5.
+        else if (Input.GetKeyDown(KeyCode.Alpha5) && (Camera1.enabled == true || Camera2.enabled == true || Camera3.enabled == true || Camera4.enabled == true || Camera6.enabled == true))
+        {
+            Camera1.enabled = false;
+            Camera2.enabled = false;
+            Camera3.enabled = false;
+            Camera4.enabled = false;
+            Camera5.enabled = true;
+            Camera6.enabled = false;
+        }
+        // Camera 6.
+        else if (Input.GetKeyDown(KeyCode.Alpha6) && (Camera1.enabled == true || Camera2.enabled == true || Camera3.enabled == true || Camera4.enabled == true || Camera5.enabled == true))
+        {
+            Camera1.enabled = false;
+            Camera2.enabled = false;
+            Camera3.enabled = false;
+            Camera4.enabled = false;
+            Camera5.enabled = false;
+            Camera6.enabled = true;
         }
     }
 }
