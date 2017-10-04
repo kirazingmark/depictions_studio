6f2274e20bf6b98686261fe3047ba1af21b6e06a
SAE\hyanuary <1003495@student.sae.edu.au>
2017-10-04 18:07:38 +1000

    exit button is placed on the camera

---
+++ Assets/Painting/Updated2/TexturePainter/Scripts/ControlPaint.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ControlPaint : MonoBehaviour {
+
+    CameraSwitcher pCamera;
+    public Button exitButton;
+
+	// Use this for initialization
+	void Start () {
+        pCamera = GameObject.FindGameObjectWithTag("Player").GetComponent<CameraSwitcher>();
+		
+	}
+
+    void Update()
+    {
+        if (pCamera.Camera7.enabled == true)
+            exitButton.gameObject.SetActive(true);
+        else
+            exitButton.gameObject.SetActive(false);
+    }
+
+    public void exitPainting() //exiting the painting part
+    {
+        pCamera.Camera7.enabled = false;
+    }
+}
