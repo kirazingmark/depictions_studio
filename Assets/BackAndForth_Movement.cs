6890f7b22a4d682abf29403c0fcf5f665d0df5d9
Kira Zingmark <kirazingmark@gmail.com>
2017-10-21 23:52:44 +1000

    Minor Environmental Update.
    - New BackAndForth Script created to manage swinging of Clothes on Clothes Line using Sine curve.
    - BackAndForth Script applied to all Clothes items and configured (with Clothes items with larger surface area set to swing further).
    - Missing Poles for Clothes Line added to the Scene.
    - CribMobile_Spin Script created and added to Mobile above Crib in order for it to slowly spin at a rate of 5.0f in a clockwise direction.
    - Broken Glass Panel on Sliding Door of Studio moved back onto the SlidingDoor Gameobject Group, now it moves with the rest of the Door.
    - Left over light in Studio removed.
    - Other minor changes.

---
+++ Assets/BackAndForth_Movement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackAndForth_Movement : MonoBehaviour {
+
+    public float _Angle;
+    public float _Period;
+
+    private float _Time;
+
+    // Update is called once per frame
+    void Update () {
+        _Time = _Time + Time.deltaTime;
+        float phase = Mathf.Sin(_Time / _Period);
+        transform.localRotation = Quaternion.Euler( new Vector3(phase * _Angle, 0,0));
+    }
+}
