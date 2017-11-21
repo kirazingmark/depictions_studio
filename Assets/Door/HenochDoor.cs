1007bc11ac13d260589e97ae433fa0f2310caa15
Henoch Yanuary <1003495@student.sae.edu.au>
2017-11-21 19:46:25 +1000

    new door script as a prefab

---
+++ Assets/Door/HenochDoor.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HenochDoor : MonoBehaviour
+{
+
+    private bool isOpen = false;
+    private float timer = 5.0f;
+    Animation anim;
+    CameraSwitcher pCamera;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        anim = this.gameObject.GetComponent<Animation>();
+        pCamera = GameObject.FindGameObjectWithTag("Player").GetComponent<CameraSwitcher>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int x = Screen.width / 2;
+        int y = Screen.height / 2;
+
+        Ray ext = pCamera.Camera1.ScreenPointToRay(new Vector3(x, y));
+        RaycastHit hit;
+        if (Physics.Raycast(ext, out hit))
+        {
+            if (hit.collider.gameObject.tag == "Door")
+            {
+                Debug.Log(hit.collider.gameObject.tag);
+                if (Input.GetKeyDown(KeyCode.E) && isOpen == false)
+                {
+                    anim.Play("Open");
+                    isOpen = true;
+                }
+
+            }
+        }
+        if (isOpen)
+        {
+            timer -= Time.deltaTime;
+        }
+        if (timer <= 0)
+        {
+            anim.Play("Close");
+            isOpen = false;
+            timer = 5.0f;
+        }
+
+    }
+}
+
