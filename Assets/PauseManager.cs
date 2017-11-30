a068869a7f4c0494266b92d71a69703365c8dc6a
Kira Zingmark <kirazingmark@gmail.com>
2017-12-01 07:04:32 +1000

    Pause Menu Updates.
    - Pause Functionality working, requires a small fix to get mouse movement working properly (will ask Henoch today).

---
+++ Assets/PauseManager.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseManager : MonoBehaviour {
+
+    // VARIABLES AND CONSTANTS.
+    public GameObject PausePanel;
+    public string playMainMenu;
+    public string playCredits;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        PausePanel.SetActive(false);
+        Cursor.visible = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Time.timeScale == 1)
+            {
+                Time.timeScale = 0;
+                PausePanel.SetActive(true);
+                Cursor.visible = true;
+                // Screen.lockCursor = false; // Old, depricated function.
+                Cursor.lockState = CursorLockMode.None;
+            }
+            else
+            {
+                Time.timeScale = 1;
+                PausePanel.SetActive(false);
+                Cursor.visible = false;
+                Screen.lockCursor = true; // Old, depricated function.
+                Cursor.lockState = CursorLockMode.Locked;
+            }
+        }
+	}
+
+    // Return to Main Game.
+    public void PlayResume()
+    {
+
+        Time.timeScale = 1;
+        PausePanel.SetActive(false);
+        Cursor.visible = false;
+        Screen.lockCursor = true; // Old, depricated function.
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Return to Main Menu.
+    public void PlayMainMenu()
+    {
+        Cursor.visible = true;
+        SceneManager.LoadScene(playMainMenu);
+    }
+
+    // Open Crediits.
+    public void PlayCredits()
+    {
+        Cursor.visible = false;
+        SceneManager.LoadScene(playCredits);
+    }
+
+    // Quit game function.
+    public void QuitGame()
+    {
+
+        Application.Quit();
+    }
+}
